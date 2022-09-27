4ec5df7d9f43635675f200d5195047ae15a23fb0
yun <yun@DESKTOP-VQJVDIT>
2022-09-27 21:50:08 +0900

    09/27 수업자료

--- src/day04/constructor/mypac/HandPhone.java
+++ src/day04/constructor/mypac/HandPhone.java
@@ -5,14 +5,14 @@ public class HandPhone {
 	
 	// 디폴트 생성자
 	public HandPhone() {
-		model = "갤럭시";
+		this.model = "갤럭시";
 		System.out.println("디폴트 생성자가 호출되었습니다.");
 	}
 	
 	// 오버로딩 생성자
-	public HandPhone(String name) {
-		model = name;
-		System.out.println("생성자로" + name + "이 전달되었습니다");
+	public HandPhone(String model) {
+		this.model = model;
+		System.out.println("생성자로" + model + "이 전달되었습니다");
 	}
 
 }
