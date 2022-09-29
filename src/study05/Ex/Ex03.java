a21a9f06de02c9807ccdf53781a12854684b17f2
yun <yun@DESKTOP-VQJVDIT>
2022-09-29 19:04:05 +0900

    문법정리 및 문제풀이

---
+++ src/study05/Ex/Ex03.java
@@ -0,0 +1,40 @@
+package study05.Ex;
+
+/*
+3번
+    *
+   ***
+  *****
+ *******
+*********
+ *******
+  *****
+   ***
+    *
+*/
+
+public class Ex03 {
+	public static void main(String[] args) {
+		
+		for(int i = 1; i < 10; i+=2) {
+			for(int j = 9; j > i; j-=2) {
+                System.out.print(" ");
+			}
+			for(int j = 0; j < i; j++) {
+				System.out.print("*");
+			}
+			System.out.println();
+		}
+		
+		for(int i = 1; i < 10; i+=2) {
+			for(int j = 0; j < i; j+=2) {
+				System.out.print(" ");
+			}
+			for(int j = 8; j > i; j--) {
+				System.out.print("*");
+			}
+			System.out.println();
+		}
+	}
+
+}
---
+++ src/study05/MultiTable.java
@@ -0,0 +1,22 @@
+/*
+package study05;
+
+// 구구단을 표시 다중루프
+
+public class MultiTable {
+	public static void main(String[] args) {
+		for (int i= 1; i <= 9; i++) {
+			for(int j = 1; j <= 9; j++) {
+				if ( i * j < 10) {
+					System.out.print("  ");
+				} else{
+					System.out.print(" ");
+				}
+				System.out.print(i * j);
+			}
+			System.out.println();
+		}
+	}
+
+}
+*/
