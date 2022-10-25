e7868aef1d0eee7b4c3db8e7d23c0e0ae9a67013
yun <yun@DESKTOP-VQJVDIT>
2022-10-25 21:28:39 +0900

    10/25 수업자료

--- src/day10/jdbc/MainClass02.java
+++ src/day10/jdbc/MainClass02.java
@@ -6,48 +6,48 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
 
-public class MainClass01 {
+public class MainClass02 {
 	public static void main(String[] args) throws SQLException {
 		Connection conn = null;
 		Statement stmt = null;
 		ResultSet rs = null;
-		
+
 		try {
 			// 1. 드라이버 로딩
 			Class.forName("com.mysql.jdbc.Driver");
-						
-			// 2. 연결하기
+		
+     		// 2. 연결하기
 			// jdbc:mysql://호스트URL:포트/데이터베이스명
 			String url = "jdbc:mysql://172.16.12.101:3306/hr";
 			conn = DriverManager.getConnection(url, "scott", "tiger");
-						
+				
 			// 3. 쿼리 수행을 위한 Statement 객체 생성
 			stmt = conn.createStatement();
-					
-			String deptNo = "20";
-			
-			// 4. SQL 쿼리작성
-			String sql = "SELECT * FROM emp";
 						
+			// 4. SQL 쿼리작성
+			String sql = "INSERT INTO emp (EMPNO, ENAME, JOB, MGR, HIREDATE, SAL, COMM, DEPTNO) "
+			+ "VALUES(9998, 'PIKA', 'SALESMAN', 7788, '2022-10-25', 1000, 10, 30)";
+			
 			// 5. 쿼리 수행
-			rs = stmt.executeQuery(sql);
+			int result = stmt.executeUpdate(sql);
+						
 						
 			// 6. 실행결과 출력하기
-			while (rs.next()) {
-			String empno = rs.getString(1);
-			String ename = rs.getString(2);
-			String job = rs.getString(3);
-							
-			System.out.println(empno +" / "+ename+ " / "+ job);
+			System.out.println("result : " + result);
 				
 			}
 			
-		} catch (Exception e) {
-			e.printStackTrace();
+		catch (Exception e) {
+			// TODO: handle exception
 		} finally {
-			if(rs != null) rs.close();
-			if(stmt != null) stmt.close();
-			if(conn != null) conn.close();
+			if (rs != null && !rs.isClosed()) {
+				rs.close();
+			}if (stmt != null && !stmt.isClosed()) {
+				rs.close();
+			}if (conn != null && !conn.isClosed()) {
+				rs.close();
+			}
 		}
 	}
+
 }
